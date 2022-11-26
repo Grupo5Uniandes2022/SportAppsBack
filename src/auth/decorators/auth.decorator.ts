3b1df7bc10e6f5bb626977fd23d0301ea8cb586e
carlos-castiblanco <carlos.castiblanco@globant.com>
2022-11-26 17:07:50 -0500

    :construction: CC Fix testing

--- src/auth/decorators/auth.decorator.ts
+++ src/auth/decorators/auth.decorator.ts
@@ -1,13 +1,10 @@
-/* istanbul ignore file */
 import { applyDecorators, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { UserRoleGuard } from '../guards/user-role.guard';
 import { ValidRoles } from '../interfaces';
 import { RoleProtected } from './role-protected.decorator';
 
-/* istanbul ignore next */
 export function Auth(...roles: ValidRoles[]) {
-  /* istanbul ignore next */
   return applyDecorators(
     RoleProtected(...roles),
     UseGuards(AuthGuard(), UserRoleGuard),
