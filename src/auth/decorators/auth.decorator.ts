ae3acbff56e7432685362f8a25b552224c9e1969
carlos-castiblanco <carlos.castiblanco@globant.com>
2022-11-26 16:47:32 -0500

    :construction: CC Fix testing

--- src/auth/decorators/auth.decorator.ts
+++ src/auth/decorators/auth.decorator.ts
@@ -5,6 +5,7 @@ import { UserRoleGuard } from '../guards/user-role.guard';
 import { ValidRoles } from '../interfaces';
 import { RoleProtected } from './role-protected.decorator';
 
+/* istanbul ignore next */
 export function Auth(...roles: ValidRoles[]) {
   return applyDecorators(
     RoleProtected(...roles),
